c360475bb58d499e3e687bb6d1af41663c42a5f9
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2023-08-29 09:31:57 -0700

    added rest of well images

--- src/components/project-pages/Well.js
+++ src/components/project-pages/Well.js
@@ -10,7 +10,7 @@ import TwoColumnImageLayout from "../project-sections/TwoColumnImageLayout"
 export default function Well() {
 
 
-    const researchCarouselImageData = [
+    const researchImages = [
         {
             imageTitle: "kettleUser.jpg",
             altText: "picture of earl grey who likes tea"
@@ -18,6 +18,36 @@ export default function Well() {
         {
             imageTitle: "kettle-task-analysis-1-4.jpeg",
             altText: "series of pictures showing earl grey pouring tea"  
+        }
+    ]
+
+    const rapidVisImages = [
+        {
+            imageTitle: "oomen_napkin_sketches.jpeg",
+            altText: "sketched of kettle variations"
+        },
+        {
+            imageTitle: "electrickettle-07.jpg",
+            altText: "paper prototype kettle variations"  
+        }
+    ]
+
+    const ideationSketchingImages = [
+        {
+            imageTitle: "Sketching2.jpeg",
+            altText: "Shaded sketches of flat kettle"  
+        },
+        {
+            imageTitle: "Frame12.jpeg",
+            altText: "Outline sketches of flat kettle"  
+        },
+        {
+            imageTitle: "kettlevariations-01.jpg",
+            altText: "Sketched kettle variations"
+        },
+        {
+            imageTitle: "twistdiagram.jpeg",
+            altText: "Various angles of kettle twisting"  
         }
     ]
 
@@ -30,14 +60,24 @@ export default function Well() {
                 duration="5 WEEKS"
                 date="NOV - DEC 2022"
             /> 
-            <FullWidthImage project="Well" imageName="blackkettlesideboiling.jpg" altText="side view of a black kettle boiling"/>
-            <div className="small-spacer"></div>
+
+            <FullWidthImage project="Well" imageName="blackkettlesideboiling.jpg" altText="Side view of a black kettle boiling"/>
+
             <SectionHeader text="RESEARCH" />
-            <ImageCarousel project="Well" imageData={researchCarouselImageData} />
+            <ImageCarousel project="Well" imageData={researchImages} />
+
             <SectionHeader text="INSPIRATION" />
-            <FullWidthImage project="Well" imageName="Mood_Boards.jpeg" altText="collage of images showing sophisticated and colorful interiors and sleek metal kettles"/>
+            <FullWidthImage project="Well" imageName="Mood_Boards.jpeg" altText="Collage of images showing sophisticated and colorful interiors and sleek metal kettles"/>
+
             <SectionHeader text="RAPID VISUALIZATION" />
-            <TwoColumnImageLayout project="Well" imageData={researchCarouselImageData} />
+            <TwoColumnImageLayout project="Well" imageData={rapidVisImages} />
+
+            <SectionHeader text="IDEATION SKETCHING" />
+            <TwoColumnImageLayout project="Well" imageData={ideationSketchingImages} />
+            <FullWidthImage project="Well" imageName="kettlecablemanagement.19.jpg" altText="View of kettle base" />
+            <FullWidthImage project="Well" imageName="kettleexplode.jpg" altText="Expanded view of kettle" />
+            <FullWidthImage project="Well" imageName="WellColorLine.jpg" altText="Examples of kettles in different colors" />
+
             <Navbar />
             <MainSplash />
         </div>

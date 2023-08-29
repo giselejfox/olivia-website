51b9b6dfbc32c21e9fc298d53a25fbf23814b1f8
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2023-08-29 09:00:53 -0700

    added name animation and images

--- src/components/MainSplash.js
+++ src/components/MainSplash.js
@@ -4,9 +4,49 @@ import { Link } from "react-router-dom"
 
 export default function MainSplash() {
     return (
-        <div className="main-splash d-flex flex-column justify-content-end">
+        <div className="main-splash d-flex flex-column justify-content-center align-items-center">
+            <NameAnimation />
             <ProjectBar />
         </div>
+    )
+}
+
+function NameAnimation() {
+
+    const firstName = "OLIVIA"
+    const lastName = "OOMEN"
+
+    const moveColorLetter = (e) => {
+        // Change it's color and position to random options
+        e.target.style.color = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6)
+        e.target.style.top = -30 + Math.random() * 60 + "px";
+        e.target.style.left = -30 + Math.random() * 60 + "px";
+
+        // Change it back after a certain period of time
+        setTimeout(function(){
+            e.target.removeAttribute("style")
+        }, 1000);
+    }
+
+    const firstNameElements = firstName.split('').map((letter) => {
+        return <div onMouseOver={moveColorLetter} className="fw-bold main-splash-letter">{letter}</div>
+    })
+
+    const lastNameElements = lastName.split('').map((letter) => {
+        return <div onMouseOver={moveColorLetter} className="fw-bold main-splash-letter">{letter}</div>
+    })
+
+    return (
+        <h1 className="pb-4">
+            <div className="d-flex flex-column align-items-center justify-content-center">
+                <div className="d-flex flex-row">
+                    {firstNameElements}
+                </div>
+                <div className="d-flex flex-row">
+                    {lastNameElements}
+                </div>
+            </div>
+        </h1>
     )
 }
 
@@ -14,19 +54,19 @@ function ProjectBar() {
     return (
         <div className="d-flex flex-wrap py-4 justify-content-center">
             <div className=" mx-2">
-                <Link className="main-splash-link fw-bold" to='well'>WELL</Link>
+                <Link className="main-splash-link fw-bold" to='/well'>WELL</Link>
             </div>
             <div className="mx-2">
-                <Link className="main-splash-link fw-bold" to='beam'>BEAM</Link>
+                <Link className="main-splash-link fw-bold" to='/beam'>BEAM</Link>
             </div>
             <div className="mx-2">
-                <Link className="main-splash-link fw-bold" to='spur'>SPUR</Link>
+                <Link className="main-splash-link fw-bold" to='/spur'>SPUR</Link>
             </div>
             <div className="mx-2">
-                <Link className="main-splash-link fw-bold" to='conversation-ave'>CONVERSATION AVE</Link>
+                <Link className="main-splash-link fw-bold" to='/conversation-ave'>CONVERSATION AVE</Link>
             </div>
             <div className="mx-2">
-                <Link className="main-splash-link fw-bold" to='site-archive-cite'>SITE / ARCHIVE / SITE</Link>
+                <Link className="main-splash-link fw-bold" to='/site-archive-cite'>SITE / ARCHIVE / SITE</Link>
             </div>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,17 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg py-5 px-4">
             <div className="container-fluid">
-                <Link className="navbar-brand fw-bold" to="work">OLIVIA OOMEN</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <Link className="navbar-brand fw-bold" to="/work">OLIVIA OOMEN</Link>
+                <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end align-middle" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <Link className="nav-link active fw-bold" aria-current="page" to="work">WORK</Link>
+                        <Link className="nav-link active fw-bold" aria-current="page" to="/work">WORK</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link active fw-bold"to="about">ABOUT</Link>
+                        <Link className="nav-link active fw-bold"to="/about">ABOUT</Link>
                     </li>
                 </ul>
                 </div>

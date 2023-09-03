77b033baca6d10952069e4a82ca6f889fb5c0759
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2023-09-02 22:06:47 -0700

    removing ranking from visibility on small screens

--- src/components/sole-opinion-sections/HeroSection.js
+++ src/components/sole-opinion-sections/HeroSection.js
@@ -7,7 +7,7 @@ export default function HeroSection() {
                 <span>SOLE</span> 
                 <span>OPINION</span>
             </h1>
-            <p className="fw-bold">Some example intro text to put up in Here</p>
+            <p className="fw-bold w-75">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     )
 }

aaed7aff8dad0ee72184aa191d8c38763e313232
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2023-12-08 20:32:37 -0800

    liv's gravity sketch works

--- src/components/name-animations/CursorFollow3D.js
+++ src/components/name-animations/CursorFollow3D.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useMemo } from 'react';
 import { Canvas, useFrame } from 'react-three-fiber';
 import { STLLoader } from 'three/addons/loaders/STLLoader';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
@@ -6,31 +6,58 @@ import { Box, OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
 
 
-function Model({ group }) {
+function STLModel({ group }) {
   
   // Load the STL model
-  const loader = new GLTFLoader();
-  const glbPath = 'img/Peter.glb';
-  // loader.load(glbPath, (geometry) => {
-  //   const material = new THREE.MeshNormalMaterial({
-  //     // transparent: true,
-  //     // opacity: 0.7, // Adjust the opacity as needed
-  //   });
-  //   const mesh = new THREE.Mesh(geometry, material);
+  const loader = new STLLoader();
+  // const loader = new GLTFLoader();
+  const modelPath = 'img/ShoeModel.stl';
 
-  //   // Scale down the model
-  //   // mesh.scale.set(0.2, 0.2, 0.2); // Adjust the scale factor as needed
+  // FOR THE SHOE
+  loader.load(modelPath, (geometry) => {
+    const material = new THREE.MeshNormalMaterial({
+      transparent: true,
+      opacity: 0.7, // Adjust the opacity as needed
+    });
+    const mesh = new THREE.Mesh(geometry, material);
 
-  //   group.current.add(mesh);
+    // Scale down the model
+    mesh.scale.set(0.2, 0.2, 0.2); // Adjust the scale factor as needed
 
-  // });
-  loader.load(glbPath, (gltf) => {
-    gltf.scene.scale.set(10, 10, 10);
-    group.current.add(gltf.scene);
+    group.current.add(mesh);
+
   });
 
   return <group ref={group} />;
+}
 
+function GLTFModel({ group }) {
+  const modelPath = 'img/3D+Shoe+Sketch+by+Archie+RefImage+Copy@08122023-195607.glb';
+
+  const loader = useMemo(() => new GLTFLoader(), []);
+
+  useEffect(() => {
+    const loadModel = async () => {
+      const gltf = await loader.loadAsync(modelPath);
+      const clonedScene = gltf.scene.clone();
+      group.current.add(clonedScene);
+
+      // Adjust the scale factor as needed
+      clonedScene.scale.set(1.5, 1.5, 1.5);
+
+      // Rotate the model 180 degrees along the x-axis
+      clonedScene.rotation.x = Math.PI; // Math.PI represents 180 degrees
+      clonedScene.rotation.z = Math.PI; // 180 degrees around the z-axis)
+    };
+
+    loadModel();
+  }, [loader, modelPath, group]);
+
+  useFrame(() => {
+    // Add any animation or frame-specific logic here
+  });
+
+  return <group ref={group} />;
 }
 
 export default function CursorFollow3D() {
@@ -39,17 +66,27 @@ export default function CursorFollow3D() {
   const updateShoeRotation = (event) => {
     const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
     const mouseY = (event.clientY / window.innerHeight) * 2 - 1;
-  
-    group.current.rotation.x = mouseY * Math.PI * 2; // Adjust the scale factor as needed
-    group.current.rotation.y = mouseX * Math.PI * 2; // Adjust the scale factor as needed
+
+    group.current.rotation.x = mouseY * Math.PI * 1; // Adjust the scale factor as needed
+    group.current.rotation.y = mouseX * Math.PI * 1; // Adjust the scale factor as needed
+
   };
 
   return (
-    <Canvas style={{ background: 'white' }} onMouseMove={updateShoeRotation}>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Model group={group} />
-      <OrbitControls />
-    </Canvas>
+    <div className="canvas-container">
+      <Canvas style={{ background: 'white' }} onMouseMove={updateShoeRotation}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        {/* <STLModel group={group} /> */}
+        <GLTFModel group={group}/>
+        <OrbitControls />
+      </Canvas>
+      <div className="heading-container">
+        <h1 className='main-name-3D-backdrop d-flex flex-column align-items-center'>
+          <span>OLIVIA</span>
+          <span>OOMEN</span> 
+        </h1>
+      </div>
+    </div>
   );
 };

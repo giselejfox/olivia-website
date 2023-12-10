0c0e9c58aca19450cf3181bdc1629c8cca9c1736
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2023-12-10 09:53:38 -0800

    the balls can be dragged and push each other around

--- src/components/project-pages/Spur.js
+++ src/components/project-pages/Spur.js
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect, useMemo } from 'react';
 import * as THREE from 'three';
-import { World, Sphere, Body, Vec3 } from 'cannon';
 
 const TwoSpheresDragging = () => {
     const camera = useMemo(() => new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000), []);
@@ -114,83 +113,6 @@ const TwoSpheresDragging = () => {
   
     return <div style={{ width: '100%', height: '100%' }} />;
   };
-  
-function ThreeDComponent() {
-    const containerRef = useRef();
-  
-    useEffect(() => {
-  
-      // Create a local variable to store the reference
-      const container = containerRef.current;
-  
-      // Set up Three.js scene
-      const scene = new THREE.Scene();
-      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-      const renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      container.appendChild(renderer.domElement);
-  
-      // Create 3D objects (e.g., cubes)
-      const geometry = new THREE.BoxGeometry();
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      const cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
-  
-      // Set up mouse interaction
-      const mouse = new THREE.Vector2();
-      const raycaster = new THREE.Raycaster();
-      window.addEventListener('mousemove', onMouseMove, false);
-  
-      function onMouseMove(event) {
-        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-      }
-  
-      // Animation loop
-      const animate = () => {
-        requestAnimationFrame(animate);
-  
-        // Update cube position based on mouse movement
-        raycaster.setFromCamera(mouse, camera);
-        const intersects = raycaster.intersectObjects(scene.children);
-        if (intersects.length > 0) {
-          const intersect = intersects[0];
-          const { x, y, z } = intersect.point;
-          cube.position.set(x, y, z);
-        }
-  
-        // Rotate cube
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-  
-        // Render the scene
-        renderer.render(scene, camera);
-      };
-  
-      // Handle window resize
-      window.addEventListener('resize', () => {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-      });
-  
-      // Initial setup
-      camera.position.z = 5;
-      animate();
-  
-      // Cleanup on component unmount
-      return () => {
-          window.removeEventListener('mousemove', onMouseMove);
-           
-          // Use the local container variable instead of containerRef.current
-          if (container) {
-              container.removeChild(renderer.domElement);
-          }
-      };
-    }, []); // Empty dependency array ensures the effect runs once on mount
-  
-    return <div ref={containerRef} />;
-};
 
 export default function Spur() {
     return (

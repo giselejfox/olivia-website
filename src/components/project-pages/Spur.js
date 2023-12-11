46e1c7ebd7e433eef0ada8a862fe6b556671d034
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2023-12-10 16:45:32 -0800

    rename pics and little cleanup

--- src/components/project-pages/Spur.js
+++ src/components/project-pages/Spur.js
@@ -1,16 +1,9 @@
-import React, { useRef, useEffect, useMemo } from 'react';
-import { World, Body, Sphere, Vec3 } from 'cannon';
-import * as THREE from 'three';
-
-import TwoSpheresBouncingCannon from './TEMP-animation-tests/TwoSpheresBouncingCannon';
-import TwoSpheresDragging from './TEMP-animation-tests/TwoSpheresDragging';
+import React from 'react';
 
 export default function Spur() {
     return (
         <div>
             <h1>I'm here</h1>
-            <TwoSpheresDragging />
-            {/* <TwoSpheresBouncing /> */}
         </div>
     )
 }

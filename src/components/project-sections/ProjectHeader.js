cb7b11b09f6d57f9543cd15a6e02068adf3749e1
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2024-01-08 15:17:01 -0800

    clean up code

--- src/components/project-sections/ProjectHeader.js
+++ src/components/project-sections/ProjectHeader.js
@@ -12,12 +12,6 @@ export default function ProjectHeader(props) {
         <div className="container py-5">
             <h1 className="mb-4">{title}</h1>
             <div className="d-flex flex-column flex-md-row flex-wrap justify-content-between fw-bold">
-                {/* <div className="pb-3 pb-lg-0 col-md-8 pe-3">{description}</div>
-                <div className="d-flex flex-column flex-md-row flex-wrap justify-content-end col-md-4">
-                    <div className="px-0 pb-3 pb-lg-0 pe-md-3">{projectType}</div>
-                    <div className="px-0 pb-3 pb-lg-0 pe-md-3">{duration}</div>
-                    <div className="px-0 pb-3 pb-lg-0 pe-md-3">{date}</div>
-                </div> */}
                 <div className="pb-3 pb-lg-0 col-12 col-md-8 pe-5">{description}</div>
                 <div className="px-0 pb-3 pb-lg-0 pe-md-4 col-12 col-md-2">{projectType}</div>
                 <div className="px-0 pb-3 pb-lg-0 pe-md-4 col-12 col-md-1">{duration}</div>

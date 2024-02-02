6bb5db5271baa1c4449831e570003852b435c5e8
Olivia Oomen <oliviaaver7@gmail.com>
2024-02-02 12:04:01 -0800

    honors project: popups change colors, added scroll bar

--- src/components/honors-project-components/ProjectModal.js
+++ src/components/honors-project-components/ProjectModal.js
@@ -10,10 +10,40 @@ This build out the modal that pops up when you click on an icon
 */
 
 export default function ProjectModal({showModal, modalContentTitle, handleCloseModal}) {
+    
+  const getCustomStyles = () => {
+    switch (modalContentTitle) {
+      case 'bottle':
+        return { backgroundColor: '#00555A', color: 'white' };
+      case 'sewing-machine':
+        return { backgroundColor: '#910D0D', color: 'white' };
+      case 'fabric':
+        return { backgroundColor: '#720D57', color: 'white' };
+      case 'laptop':
+        return { backgroundColor: '#4B0062', color: 'white' };
+      case 'record-player':
+        return { backgroundColor: '#001D6E', color: 'white'};
+      case 'newspaper':
+        return { backgroundColor: '#AA3500', color: 'white'};
+      case 'printer':
+        return { backgroundColor: '#0D4E01', color: 'white'};
+      case 'games':
+        return { backgroundColor: '#522D06', color: 'white'};
+      case 'headphones':
+        return { backgroundColor: '#837107', color: 'white'};
+      // Add more cases for other modalContentTitles as needed
+      default:
+        return { backgroundColor: 'white', color: 'black' }; // Default styles
+    }
+  };
+
+  const customStyles = getCustomStyles();
+  
     return (
       <Modal show={showModal} onHide={handleCloseModal} dialogClassName="custom-modal">
-        <Modal.Body><ModalBodyContent contentTitle={modalContentTitle} /></Modal.Body>
-        <Modal.Footer>
+        <Modal.Body style={{ ...customStyles, maxHeight: '80vh', overflowY: 'auto'}}>
+        <ModalBodyContent contentTitle={modalContentTitle} /></Modal.Body>
+        <Modal.Footer style={{ ...customStyles, border: 'none' }}>
           <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
         </Modal.Footer>
       </Modal>
--- src/components/honors-project-components/modal-contents/SewingmachineContent.js
+++ src/components/honors-project-components/modal-contents/SewingmachineContent.js
@@ -4,11 +4,15 @@ export default function SewingmachineContent() {
         fontFamily: 'DM Sans, sans-serif', // Specify DM Sans as the font-family
     };
 
-    
     return (
         <div style={customStyles}>
             <h2 className="text-center">Sewing Machine Content</h2>
-            <div>Sewing woooooo!</div>
+            <div>As a daughter of educators, I’ve always considered myself a lifelong learner, and valued the opportunity to educate myself, even if in unconventional ways. I started to embody this when my first day of college unfolded within the familiar confines of my home, as the COVID-19 pandemic reshaped traditional learning structures. Entering college in unexpected circumstances strengthened my ability to independently nurture interests and grow skills without following a conventional structure of learning. 
+
+During my transformative journey at the University of Washington, I looked past the conventional boundaries of design, evolving to gain an understanding of my positionality within the design landscape. Beyond the acquisition of technical skills, my education became a rich tapestry woven with diverse passions, unraveling the intricate relationship between products and the world that envelops them. In clubs, coursework, internships, and research experiences, I experienced the profound impact of collaboration, discovering that the synergy of collective effort could yield results beyond the sum of individual contributions. The relationships I made through collaborative projects not only shaped my creative process but also honed my communication abilities, emphasizing the invaluable role of teamwork in the realm of design.
+My perspective underwent a metamorphosis, influenced by interests such as sustainability, accessibility, and material science. I ventured into domains like exhibition design, interior design, accessible technology, and footwear design, discovering the nuanced intersections between seemingly different fields. This interdisciplinary exploration allowed me to seamlessly integrate my passions and creative thinking into different realms, extending beyond the confines of traditional coursework. My commitment to sustainability prompted me to experiment with cutting-edge practices, including the creation of designs using biodegradable materials and the adoption of responsible fabrication methods.
+In tandem with my professional growth, I recognized the profound importance of balance in my personal life. Cultivating a healthy relationship with work, I practiced effective time management, ensuring not only the pursuit of academic success but also the enrichment of my personal experiences. Investing in my personal life not only fortified my self-awareness but also clarified my values and aspirations for the future. As I embark on the next chapter of my journey, armed with technical abilities and a multifaceted understanding of design's impact, I am positioned to contribute meaningfully to the ever-evolving world of design.
+</div>
             {/* Other content specific to SewingmachineContent */}
         </div>
     )

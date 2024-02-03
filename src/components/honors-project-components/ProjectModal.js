5fe348c7340274faedbfcaf9d57d85964cfe1829
Olivia Oomen <oliviaaver7@gmail.com>
2024-02-02 16:04:31 -0800

    filling in honors popups

--- src/components/honors-project-components/ProjectModal.js
+++ src/components/honors-project-components/ProjectModal.js
@@ -41,6 +41,10 @@ export default function ProjectModal({showModal, modalContentTitle, handleCloseM
     switch (title) {
       case 'sewing-machine':
         return 'Sewing Machine';
+      case 'laptop':
+        return 'System.out.println(“Who says coding can’t be creative?”);';
+      case 'fabric':
+        return 'Materials Matter!';
       // Add cases for other titles as needed
       default:
         return title; // Use the title as is for the default
--- src/components/honors-project-components/modal-contents/FabricContent.js
+++ src/components/honors-project-components/modal-contents/FabricContent.js
@@ -1,5 +1,10 @@
 export default function FabricContent() {
     return (
-        <div>Fabric woooooo!</div>
+        <div>
+        <div>Material sustainability has become a huge passion of mine that’s formed over the past years. I started learning about the impact materials have on our health by watching movies like “The Devil We Know,” and reading books like “Fibershed: Growing a Movement of Farmers, Fashion Activists, and Makers for a New Textile Economy.” Through these, I started to think about the ways in which the materials of our products impact our lives in ways we don’t often consider.</div>
+        <img className="w-100" src='img/honors-project/artifact-images/website.jpg' alt='Website' />
+        <div>I was also able to learn about materials in different professional settings. When I was an interior design intern, I managed a materials library. That experience was formative for me to expose myself to different types of materials and grow a better understanding of why certain materials are used for certain pieces of furniture and products. After, my interest in textiles led me to a Materials Development internship at REI. While I was there, I worked on starting a database of alternative, sustainable dye processes. Being able to talk to international mill partners allowed me to gain insight on emerging dye technologies within an industrial setting. </div>
+        <div>As a designer, considering material sustainability has become a pillar of my process. Whether I’m upcycling materials, reducing waste through construction processes, or experimenting with biobased materials, I’m considering how to reduce harm on our environment when creating.</div>
+    </div>
     )
 }

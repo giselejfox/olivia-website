2a467160096f9dbe4ac5621f407b63b143a9cb60
gisele <giselejfox@gmail.com>
2024-02-09 15:17:26 -0800

    coda 3D model spin and scroll

--- src/components/project-pages/Coda.js
+++ src/components/project-pages/Coda.js
@@ -1,7 +1,19 @@
 import Navbar from "../Navbar"
+import MainSplash from "../MainSplash";
+import Spacer from "../project-sections/Spacer";
 import HTMLFlipBook from "react-pageflip";
 
+import { useRef } from "react";
+import gsap from 'gsap'
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Canvas } from '@react-three/fiber';
+import { Environment, useGLTF, Float } from '@react-three/drei'
+
 import { SlickSlider } from "../project-sections/ImageCarousel";
+
+
+
 
 
 export default function Coda() {
@@ -17,11 +29,71 @@ export default function Coda() {
                 <div className="container">
                     <SlickSlider />
                 </div>
+                <SpinScrollModel />
+                <Spacer />
+                <Spacer />
+                <Spacer />
+                <Spacer />
             </div>
+            <MainSplash />
         </div>
     )
 
 }
+
+function SpinScrollModel() {
+    return(
+        <div className="d-flex flex-column justify-content-between align-items-center pb-5" style={{height: "60rem"}}>
+            <Canvas className="canvas" style={{height: "48rem", width: "48rem", background: "#FFFFFF"}} camera={{position: [0, 0, 100]}}>
+                <Environment preset="studio"/>
+                <Box />
+            </Canvas>
+            <div className="scroll-and-pin-end"></div>
+        </div>
+    )
+}
+
+function Box() {
+  
+    const meshRef = useRef();
+    
+    useGSAP(() => {
+      gsap.registerPlugin(ScrollTrigger);
+  
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          scrub: 1,
+          pin: true,
+          trigger: ".canvas",
+          start: "50% 50%",
+          endTrigger: ".scroll-and-pin-end",
+          end: "bottom 50%",
+        },
+      });
+  
+      tl.to(meshRef.current.rotation, {
+        x: 5,
+        y: 5
+      });
+  
+    })
+
+    const { nodes } = useGLTF('/img/floating-shapes/homepagesmallsize.glb')
+
+    console.log(nodes.Codatest.geometry)
+  
+    return (
+        <mesh 
+            ref={meshRef} 
+            geometry={nodes.Codatest.geometry} 
+            material={nodes.Codatest.material} 
+            scale={[2, 2, 2]} 
+            rotation={[2, .5, 0]}
+        />
+    );
+};
+
+
 
 function MyBook() {
 

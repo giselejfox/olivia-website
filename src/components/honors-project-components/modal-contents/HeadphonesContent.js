88483d57319928ea15b1b8943e1a375c49d936b5
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2024-02-25 16:11:23 -0800

    collage uploads

--- src/components/honors-project-components/modal-contents/HeadphonesContent.js
+++ src/components/honors-project-components/modal-contents/HeadphonesContent.js
@@ -1,8 +1,10 @@
 export default function HeadphonesContent() {
     return (
-        <div className="px-md-5">
-            <img className="py-3 w-100" src='img/honors-project/artifact-images/computerscreen.png' alt='Coding projects' />  
-            <p className="py-2">Throughout college I rediscovered the importance of music within my life. Playing guitar, writing songs, using midis, and producing music was not only a way to express my emotions but also a way to bring myself a sense of being in the present moment. Starting a band with one of my closest friends and writing our own music has not only been fun, but a great way to practice creative vulnerability. The creative process often demands a willingness to share ideas, even when you may feel insecure about them.</p>
+        <div>
+            <img className="w-100" src='img/honors-project/artifact-images/music-collage.jpg' alt='Coding projects' />  
+            <div className="px-md-5">
+                <p className="py-2">Throughout college I rediscovered the importance of music within my life. Playing guitar, writing songs, using midis, and producing music was not only a way to express my emotions but also a way to bring myself a sense of being in the present moment. Starting a band with one of my closest friends and writing our own music has not only been fun, but a great way to practice creative vulnerability. The creative process often demands a willingness to share ideas, even when you may feel insecure about them.</p>
+            </div>
         </div>
        
     )
--- src/components/honors-project-components/modal-contents/RecordplayerContent.js
+++ src/components/honors-project-components/modal-contents/RecordplayerContent.js
@@ -1,7 +1,7 @@
 export default function RecordplayerContent() {
     return (
         <div className="px-md-5">
-            <img className="py-3 w-100" src='img/honors-project/artifact-images/computerscreen.png' alt='Coding projects' /> 
+            <img className="py-3 w-100" src='img/honors-project/artifact-images/ethnimusicology-collage.jpg' alt='Coding projects' /> 
             <p className="py-2">Taking Honors courses related to ethnomusicology was an unexpected way for me to combine my passion for music in an academic setting. Considering the ways in which music functions in different cultures, and how music itself reacts to shifts in culture, broadened my perspective on the definition of music and its function in the world. I also gained a wider perspective for considering the design of instruments themselves, and thinking about how the evolution of design and technology has impacted music. I really enjoyed looking at different formats of music throughout history, such as wax cylinders.</p>
         </div>
     )

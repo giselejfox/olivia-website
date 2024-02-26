cae9399a1b7ac224b27992e0dd9a9357781a0c97
Olivia Oomen <oliviaaver7@gmail.com>
2024-02-25 18:09:43 -0800

    images for honors portfolio

--- src/components/honors-project-components/modal-contents/BottleContent.js
+++ src/components/honors-project-components/modal-contents/BottleContent.js
@@ -4,7 +4,7 @@ export default function BottleContent() {
     // NOTE: you have to have a div wrapping everything in the return statement!
     return (
         <div className="px-md-5">  
-            <img className="py-3 w-100" src='img/croppedsquareselfportrait.gif' alt='sketch of liv turning around' />
+            <img className="py-3 w-100" src='img/honors-project/artifact-images/studyabroad-collage.png' alt='sketch of liv turning around' />
             <p className="py-2">Bottle woooooo!</p>
         </div>
     )

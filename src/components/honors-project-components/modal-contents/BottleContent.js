dd4ff3c91dc986054662b3eb7d3805a9c100c149
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2024-02-27 21:25:35 -0800

    chahe the images names to work (png to PNG)

--- src/components/honors-project-components/modal-contents/BottleContent.js
+++ src/components/honors-project-components/modal-contents/BottleContent.js
@@ -4,7 +4,7 @@ export default function BottleContent() {
     // NOTE: you have to have a div wrapping everything in the return statement!
     return (
         <div className="px-md-5">  
-            <img className="py-3 w-100" src='img/honors-project/artifact-images/studyabroad-collage.png' alt='sketch of liv turning around' />
+            <img className="py-3 w-100" src='img/honors-project/artifact-images/studyabroad-collage.PNG' alt='sketch of liv turning around' />
             <p className="py-2">The Dopper water bottle was my favorite discovery while studying abroad in the Netherlands, where I was taking Honors courses related to innovations in museums, libraries, and archives. Along with seeing new products, I was able to grow my knowledge of decolonization within art institutions, including repatriation and institutional initiatives to build inclusive use of language surrounding artworks. Speaking with head curators and professionals from roughly 20 institutions, including the Van Gogh museum, the Netherlands Institute of Sound and Vision, Het Nieuwe Instituut, The Black Archive, and Vrije Universiteit, I grew my knowledge of innovation and decolonization within these spaces. I saw how creation is a powerful tool for changing institutions and culture. Creating new institutional structures, creating space for new narratives, creating art, creating films, creating music, creating poetry, and more are all needed for long lasting and sustainable innovation. I will be able to continue my own work as a designer with a greater international perspective and consider new approaches outside of what may be common in the United States.</p>
         </div>
     )

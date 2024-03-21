5cc77ecd6286d2ba5d01f97e05baca90270545c8
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2024-03-20 21:16:20 -0700

    add double click to project page

--- src/components/MainSplash2D.js
+++ src/components/MainSplash2D.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import React, { useEffect, useRef } from 'react';
-import { Body, Engine, Render, World, Bodies, Mouse, MouseConstraint, MatterBodies } from 'matter-js';
+import { Body, Engine, Render, World, Bodies, Mouse, MouseConstraint, Events, Runner} from 'matter-js';
+import { useNavigate } from 'react-router-dom';
 
 import Navbar from "./Navbar";
 
@@ -11,7 +12,6 @@ export default function MainSplash2D() {
             <FloatingImages2D />
             <div style={{ zIndex: 1 }} className="content-wrapper d-flex vh-100 flex-column justify-content-between">
                 <Navbar />
-                {/* <div style={{zIndex: 2}} >Test</div> */}
                 <ProjectBar />
             </div>
         </div>
@@ -33,13 +33,22 @@ function FloatingImages2D() {
     const wellRef = useRef(null);
     const textRef = useRef(null);
 
+    const navigate = useNavigate()
+
+    // const [mouseDown, setMouseDown] = useState(false)
+    // const [mouseMoveWhileDown, setMouseMoveWhileDown] = useState(false)
+
+    // This helps us double click to open the project page
+    // Will contain the time of the last body click and which body it clicked on
+    // const [lastMouseDownInfo, setLastMouseDownInfo] = useState({bodyLabel: "starter", date: 0})
+
     useEffect(() => {
 
         const engine = initializeEngine();
         const render = initializeRender(engine, canvasRef, boxRef);
         const walls = createWalls();
 
-        const createPicture = (x, y, imgURL, scale) => {
+        const createPicture = (x, y, imgURL, scale, label) => {
             return Bodies.rectangle(x, y, 100, 100, {
                 restitution: 0.6,
                 render: {
@@ -48,23 +57,23 @@ function FloatingImages2D() {
                         xScale: scale,
                         yScale: scale,
                     }
-                }
+                },
+                label: label
             });
         };
 
-        const canvasWidth = window.innerWidth; // Replace with your actual canvas width
-        const canvasHeight = window.innerHeight; // Replace with your actual canvas height
+        const canvasWidth = window.innerWidth; 
+        const canvasHeight = window.innerHeight; 
 
-        const beam = createPicture(canvasWidth * .55, canvasHeight * .3, 'img/homepage-2d-floting-images/beamhomepage.png', .3);
-        const conversationAve = createPicture(canvasWidth * .75, canvasHeight * .4, 'img/homepage-2d-floting-images/conversationavehomepage.png', .6);
-        const guitarCaseBench = createPicture(canvasWidth * .85, canvasHeight * .85, 'img/homepage-2d-floting-images/guitarcasebenchhomepage.png', .1);
-        const onsight = createPicture(canvasWidth * .1, canvasHeight * .75, 'img/homepage-2d-floting-images/onsighthomepage.png', .2);
-        const spur = createPicture(canvasWidth * .1, canvasHeight * .2, 'img/homepage-2d-floting-images/spurhomepage.png', .15);
-        const well = createPicture(canvasWidth * .5, canvasHeight * .8, 'img/homepage-2d-floting-images/wellhomepage.png', .2);
+        const beam = createPicture(canvasWidth * .55, canvasHeight * .3, 'img/homepage-2d-floting-images/beamhomepage.png', .3, "beam");
+        const conversationAve = createPicture(canvasWidth * .75, canvasHeight * .4, 'img/homepage-2d-floting-images/conversationavehomepage.png', .6, "conversation-ave");
+        const guitarCaseBench = createPicture(canvasWidth * .85, canvasHeight * .85, 'img/homepage-2d-floting-images/guitarcasebenchhomepage.png', .1, "");
+        const onsight = createPicture(canvasWidth * .1, canvasHeight * .75, 'img/homepage-2d-floting-images/onsighthomepage.png', .2, "onsight");
+        const spur = createPicture(canvasWidth * .1, canvasHeight * .2, 'img/homepage-2d-floting-images/spurhomepage.png', .15, "spur");
+        const well = createPicture(canvasWidth * .5, canvasHeight * .8, 'img/homepage-2d-floting-images/wellhomepage.png', .2, "well");
 
         const text = Bodies.rectangle(canvasWidth / 2, canvasHeight / 2, 1, 1, {
             isStatic: true, // Set the body as static
-            restitution: 0.6,
             render: {
                 sprite: {
                     texture: 'img/homepage-2d-floting-images/texthomepage.png',
@@ -96,7 +105,7 @@ function FloatingImages2D() {
             constraint: {
                 stiffness: 0.2,
                 render: {
-                    visible: true,
+                    // visible: true,
                     elementCategory: 2, // Set a unique category to the MouseConstraint
                 },
             },
@@ -107,13 +116,26 @@ function FloatingImages2D() {
             event.preventDefault();
         });
 
+        // This helps determine what's been double clicked
+        let lastMouseDownInfo = {bodyLabel: "starter", date: 0}
+
+        Events.on(mouseConstraint, "mousedown", () => {
+            // if we've clicked on a body and the last click we did was on the same body and less than 1 second ago
+            // then we open that project page
+            if (mouseConstraint.body) {
+              const doubleClick = areDatesWithinTwoSeconds(Date.now(), lastMouseDownInfo.date) && mouseConstraint.body.label === lastMouseDownInfo.bodyLabel
+              if (doubleClick) {
+                openProjectPage(navigate, mouseConstraint.body.label)
+              } else { // otherwise we rest the last thing we clicked
+                lastMouseDownInfo = {bodyLabel: mouseConstraint.body.label, date: Date.now()}
+              }
+            }
+        });
 
         World.add(engine.world, [...walls, text, beam, conversationAve, guitarCaseBench, onsight, spur, well]);
         World.add(engine.world, mouseConstraint);
-        // Add text bodies to the world
-        //   World.add(engine.world, staticText);
 
-        Engine.run(engine);
+        Runner.run(engine);
         Render.run(render);
 
     }, []);
@@ -125,6 +147,24 @@ function FloatingImages2D() {
             </div>
         </div>
     )
+}
+
+function openProjectPage(navigate, bodyLabel) {
+    if (bodyLabel.length !== 0) {
+        navigate("/" + bodyLabel)
+    }
+}
+
+function areDatesWithinTwoSeconds(date1, date2) {
+    // Convert date numbers to Date objects
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+    
+    // Get the difference in milliseconds between the two dates
+    const diff = Math.abs(d1 - d2);
+    
+    // Check if the difference is less than or equal to 2000 milliseconds (2 seconds)
+    return diff <= 1000;
 }
 
 const initializeEngine = () => {

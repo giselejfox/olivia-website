a35fd8f6d5abc195e444101be15817775db40bcb
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2024-10-01 12:09:18 -0700

    set up analytics sdk

--- src/index.js
+++ src/index.js
@@ -7,19 +7,23 @@ import { BrowserRouter } from 'react-router-dom';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBi8jfn_oj0Y85yhoSVkQZfJCSClR3sizs",
   authDomain: "liv-portfolio.firebaseapp.com",
+  databaseURL: "https://liv-portfolio-default-rtdb.firebaseio.com",
   projectId: "liv-portfolio",
   storageBucket: "liv-portfolio.appspot.com",
   messagingSenderId: "801877586389",
-  appId: "1:801877586389:web:078c6cac67ebe0166acd5c"
+  appId: "1:801877586389:web:078c6cac67ebe0166acd5c",
+  measurementId: "G-GFEXL4VZT1"
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
